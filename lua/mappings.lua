c7edb7fd1812e67b6fc8721b6a13dc59e4132ced
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2025-01-30 19:49:00 +0100

    fast quit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,4 +2,5 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>q", ":q<cr>", { desc = "quit" })
+map("n", ";", ":", { desc = "enter command mode" })
